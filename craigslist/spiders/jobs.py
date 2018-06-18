17c2823d23a399a0e47118299e0c976949471770
Sean <sean@commun-o.com>
2018-06-18 15:26:25 -0600

    url changes

--- craigslist/spiders/jobs.py
+++ craigslist/spiders/jobs.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
+# import scrapy
+
+
+# class JobsSpider(scrapy.Spider):
+#     name = 'jobs'
+#     allowed_domains = ['https://seattle.craigslist.org/d/creative-services/search/crs']
+#     # start_urls = ['https://seattle.craigslist.org/d/creative-services/search/crs/']
+#     start_urls = ['https://seattle.craigslist.org/d/creative-services/search/crs']
+
+
+#     def parse(self, response):
+#         titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+#         print(titles)
+
+# -*- coding: utf-8 -*-
 import scrapy
-
-
+ 
 class JobsSpider(scrapy.Spider):
-    name = 'jobs'
-    allowed_domains = ['https://seattle.craigslist.org/d/creative-services/search/crs']
-    # start_urls = ['https://seattle.craigslist.org/d/creative-services/search/crs/']
-    start_urls = ['https://seattle.craigslist.org/sno/crs/d/professional-affordable/6618543409.html']
-
-
+    name = "jobs"
+    allowed_domains = ["craigslist.org"]
+    start_urls = ['https://seattle.craigslist.org/d/creative-services/search/crs']
+ 
     def parse(self, response):
         titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
         print(titles)
+        for title in titles:
+          yield {'Title' : title}
